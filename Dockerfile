63a58e4a5ab186f61cbea8c2eeff89b7e813d0e2
owlinthewoods <bearinparadox@gmail.com>
2023-08-10 11:43:30 +0800

    add parseInt for port

--- Dockerfile
+++ Dockerfile
@@ -29,3 +29,9 @@ COPY --from=base /usr/src/app/dist ./dist
 EXPOSE 1339
 
 CMD [ "node", "dist/index.js" ]
+
+# docker build -t vnlkz .
+# docker run --name vnlkz -dp 1339:1339 vnlkz
+# docker ps
+# docker logs -f vnlkz
+# docker stop vnlkz
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import process from 'process';
 import {connect} from './utils/connect';
 import { writeMapsToDB, updateMapsFromKzgo, writeImpMapsToDB, writeUnfMapsToDB, writeBonusesToDB } from './utils/updateMapsFromApi';
 import { fetchWrsFromApi } from './utils/fetchWrsFromApi';
-const port = process.env.PORT || 5000;
+
+require('dotenv').config()
+
+const port = parseInt(process.env.PORT as string) || 5000;
 app.listen(port, async () => {
 	await connect();
   console.log(`Listening: http://localhost:${port}`);
